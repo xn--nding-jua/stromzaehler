742f68044f45a10f51e100fe9e25b349bf27ad5d
Christian Nöding <christian@noeding-online.de>
2020-08-18 23:54:06 +0200

    Added a new energy-bar that shows the current power

--- stromzaehler-app/app/src/main/java/de/hackerdan/stromzaehler/CurrentBroadcastUpdaterTask.java
+++ stromzaehler-app/app/src/main/java/de/hackerdan/stromzaehler/CurrentBroadcastUpdaterTask.java
@@ -9,6 +9,7 @@ import java.text.NumberFormat;
 import android.app.Activity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import de.hackerdan.sml.model.PvValue;
 
@@ -46,9 +47,29 @@ public class CurrentBroadcastUpdaterTask extends BroadcastUpdaterTask
       }
       textViewCurrent.setTextColor(activity.getResources().getColor(color));
 
+      // show sun or plant depending on current power
       activity.findViewById(R.id.sun).setVisibility(current < 0 ? View.VISIBLE : View.INVISIBLE);
       activity.findViewById(R.id.plant).setVisibility(current > 0 ? View.VISIBLE : View.INVISIBLE);
 
+      // move the energy bar depending on current power
+      RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(50, 50);
+      float Pmax=5000; // +100%
+      float Pmin=-600; // -100%
+      int leftMargin;
+      if (current<0) {
+         leftMargin = Math.round(175 - (175*(current/Pmin))); // 0=-100%, 175=0%, 350=100%
+      }else{
+         if (current<=Pmax) {
+            leftMargin = Math.round(175 + (175 * (current / Pmax))); // 0=-100%, 175=0%, 350=100%
+         }else{
+            // end of bar
+            leftMargin = 350;
+         }
+      }
+      params.setMargins(leftMargin, -100, 0, 0);
+      activity.findViewById(R.id.imageArrow).setLayoutParams(params);
+
+      // show allowed loads
       switchImage(activity, current, -100, R.id.imageCharge, R.mipmap.charge, R.mipmap.charge_off);
       switchImage(activity, current, -1500, R.id.imageWama, R.mipmap.wama, R.mipmap.wama_off);
    }
